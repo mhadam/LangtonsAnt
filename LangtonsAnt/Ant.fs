47f9feb26730e8965973e584de4cb529e7ed6283
Michael Hadam <michael@hadam.us>
2017-02-11 14:30:43 -0500

    Working in F# interactive - needs to be packaged into a console app

--- LangtonsAnt/Ant.fs
+++ LangtonsAnt/Ant.fs
@@ -9,16 +9,16 @@ let goldenRatio = (1.0 + sqrt(5.0)) / 2.0
 
 let random = Random()
 let initialColor =
-    let h = float(random.Next(100))*255.0/100.0
-    let s = 0.5 * 255.0
-    let v = 0.95 * 255.0
+    let h = float(random.Next(100))*360.0/100.0
+    let s = 0.5
+    let v = 0.95
     Hsv(H=h, S=s, V=v)
 
 let generateColor (c1 : Hsv) =
-    let next = (c1.H / 255.0 + 1.0 / goldenRatio) % 1.0
-    let s = 0.5 * 255.0
-    let v = 0.95 * 255.0
-    Hsv(H = next * 255.0, S = s, V = v)
+    let next = (c1.H / 360.0 + 1.0 / goldenRatio) % 1.0
+    let s = 0.5
+    let v = 0.95
+    Hsv(H = next * 360.0, S = s, V = v)
 
 let convertDirs (ds:string) =
     ds.ToCharArray()
@@ -27,7 +27,7 @@ let convertDirs (ds:string) =
                             | 'R' -> -90.0
                             | _ -> 0.0)
 
-let iterations = 100000
+let iterations = 10000000
 let dirs = "LRLRLRRR" |> convertDirs
 let colors = seq { yield initialColor; yield! (Seq.unfold (fun acc -> Some(generateColor acc, generateColor acc)) initialColor |> Seq.take dirs.Length) }
 
@@ -36,13 +36,7 @@ type ant = {
     position: int*int
     color: Color
     vector: float
-    grid: int[,]}
-
-let bitmap = new Bitmap(1000, 1000)
-
-for x in 0..bitmap.Width-1 do
-    for y in 0..bitmap.Height-1 do
-        bitmap.SetPixel(x, y, Color.White)
+    grid: int[,] }
 
 let dimension = (1000, 1000)
 let (x, y) = dimension
@@ -54,8 +48,8 @@ let initialAnt = {
     grid = Array2D.zeroCreate<int> x y }
 
 let moveAnt ant =
-    let { position = position } = ant
-    let (x, y) = position
+    //printfn "%A" (ant.vector, ant.position)
+    let (x, y) = ant.position
     match ant with
     | { vector = 0.0 } when x < bitmap.Width-1 -> { ant with position = (x + 1, y) }
     | { vector = 90.0 } when y < bitmap.Width-1-> { ant with position = (x, y + 1)}
@@ -65,17 +59,53 @@ let moveAnt ant =
 
 let addAngle ant angle =
     let curAngle = ant.vector
-    { ant with vector = (curAngle + angle) % 360.0 }
+    let newVector = 
+        match (curAngle + angle) % 360.0 with
+        | x when x < 0.0 -> (curAngle + angle) % 360.0 + 360.0
+        | _ -> (curAngle + angle) % 360.0
+    
+    { ant with vector = newVector }
 
 let workAnt ant =
-    let {position = position } = ant
+    let { position = position } = ant
     let (x, y) = position
     let color = Array2D.get ant.grid x y
-    let newVector = ant.vector + dirs.[color]
+    let turnedAnt = addAngle ant dirs.[color]
 
-    if color >= dirs.Length then
+    if color >= dirs.Length-1 then
         Array2D.set ant.grid x y 0
     else
         Array2D.set ant.grid x y (color+1)
 
-    moveAnt { ant with vector = newVector}
+    moveAnt turnedAnt
+
+let saveAs (name : string) (bitmap : Bitmap) =
+    let path = Path.Combine(__SOURCE_DIRECTORY__, name)
+    bitmap.Save(path)
+
+let rec callWork (ant : ant) (n : int) : ant =
+    match n with
+    | x when x > 1 -> callWork (workAnt ant) (n-1)
+    | x when x = 1 -> (workAnt ant)
+    | _ -> (workAnt ant)
+
+let final = callWork initialAnt iterations
+
+let getRGBColor (value : int) (ant : ant) =
+    let rgb = (Seq.item value colors).To<Rgb>()
+    let rgbColor = Color.FromArgb(255, int(rgb.R), int(rgb.G), int(rgb.B))
+    rgbColor
+
+let outputBitmap ant =
+    let outputBitmap =
+        new Bitmap(1000, 1000)
+
+    let pixelFunc x y v =
+        if v <> 0 then
+            printfn "%A" v
+        outputBitmap.SetPixel(x, y, (getRGBColor v ant))
+    let outputGrid =
+        Array2D.iteri pixelFunc ant.grid
+    outputBitmap
+
+saveAs "langton.png" (outputBitmap final)
