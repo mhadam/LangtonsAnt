2110fd7566935cb30dc6e90ad9c875f1717d79fd
Michael Hadam <michael@hadam.us>
2017-02-12 21:47:27 -0500

    Compiles - working command line input

--- LangtonsAnt/Ant.fs
+++ LangtonsAnt/Ant.fs
@@ -5,57 +5,60 @@ open System.IO
 open System
 open ColorMine.ColorSpaces
 
-let goldenRatio = (1.0 + sqrt(5.0)) / 2.0
+let makeColorPalette (dirs : float []) =
+    let goldenRatio = (1.0 + sqrt(5.0)) / 2.0
 
-let random = Random()
-let initialColor =
-    let h = float(random.Next(100))*360.0/100.0
-    let s = 0.5
-    let v = 0.95
-    Hsv(H=h, S=s, V=v)
+    let random = Random()
+    let initialColor =
+        let h = float(random.Next(100))*360.0/100.0
+        let s = 0.5
+        let v = 0.95
+        Hsv(H=h, S=s, V=v)
 
-let generateColor (c1 : Hsv) =
-    let next = (c1.H / 360.0 + 1.0 / goldenRatio) % 1.0
-    let s = 0.5
-    let v = 0.95
-    Hsv(H = next * 360.0, S = s, V = v)
+    let generateColor (c1 : Hsv) =
+        let next = (c1.H / 360.0 + 1.0 / goldenRatio) % 1.0
+        let s = 0.5
+        let v = 0.95
+        Hsv(H = next * 360.0, S = s, V = v)
 
-let convertDirs (ds:string) =
-    ds.ToCharArray()
-    |> Array.map (fun x -> match x with
-                            | 'L' -> 90.0
-                            | 'R' -> -90.0
-                            | _ -> 0.0)
+    let hsvColors =
+        seq { yield initialColor; yield! (Seq.unfold (fun acc -> Some(generateColor acc, generateColor acc)) initialColor |> Seq.take dirs.Length) }
+    
+    let getRGBColor (hsv : Hsv) =
+        let rgb = hsv.To<Rgb>()
+        Color.FromArgb(255, int(rgb.R), int(rgb.G), int(rgb.B))
 
-let iterations = 10000000
-let dirs = "LRLRLRRR" |> convertDirs
-let colors = seq { yield initialColor; yield! (Seq.unfold (fun acc -> Some(generateColor acc, generateColor acc)) initialColor |> Seq.take dirs.Length) }
+    Seq.map getRGBColor hsvColors
 
 type ant = {
-    dimension : int*int
+    dimensions : int*int
     position: int*int
-    color: Color
+    colors: seq<Color>
     vector: float
     grid: int[,] }
-
-let dimension = (1000, 1000)
-let (x, y) = dimension
-let initialAnt = {
-    dimension = dimension
-    position = ((fst dimension) / 2 - 1, (snd dimension) / 2 - 1)
-    color = Color.Black
-    vector = 0.0
-    grid = Array2D.zeroCreate<int> x y }
 
 let moveAnt ant =
     //printfn "%A" (ant.vector, ant.position)
     let (x, y) = ant.position
-    match ant with
-    | { vector = 0.0 } when x < bitmap.Width-1 -> { ant with position = (x + 1, y) }
-    | { vector = 90.0 } when y < bitmap.Width-1-> { ant with position = (x, y + 1)}
-    | { vector = 180.0 } when x > 0 -> { ant with position = (x - 1, y)}
-    | { vector = 270.0 } when y > 0 -> { ant with position = (x, y - 1)}
-    | _ -> ant
+    let (xLength, yLength) = (Array2D.length1 ant.grid, Array2D.length2 ant.grid)
+    let firstMove = 
+        match ant with
+        | { vector = 0.0 } when x < xLength-1 -> { ant with position = (x + 1, y) }
+        | { vector = 90.0 } when y < yLength-1-> { ant with position = (x, y + 1)}
+        | { vector = 180.0 } when x > 0 -> { ant with position = (x - 1, y)}
+        | { vector = 270.0 } when y > 0 -> { ant with position = (x, y - 1)}
+        | _ -> ant
+    
+    let (x1, y1) = firstMove.position
+
+    match firstMove.position with
+    | x, y when x = xLength && y = yLength -> { firstMove with position = (0, 0) }
+    | x, _ when x = xLength -> { firstMove with position = (0, y1) }
+    | _, y when y = yLength -> { firstMove with position = (x1, 0) }
+    | -1, -1 -> { firstMove with position = (xLength-1, yLength-1) }
+    | -1, _ -> { firstMove with position = (xLength-1, y1) }
+    | _, -1 -> { firstMove with position = (x1, yLength-1) }
+    | _, _ -> firstMove
 
 let addAngle ant angle =
     let curAngle = ant.vector
@@ -66,46 +69,51 @@ let addAngle ant angle =
     
     { ant with vector = newVector }
 
-let workAnt ant =
-    let { position = position } = ant
-    let (x, y) = position
-    let color = Array2D.get ant.grid x y
-    let turnedAnt = addAngle ant dirs.[color]
-
-    if color >= dirs.Length-1 then
-        Array2D.set ant.grid x y 0
-    else
-        Array2D.set ant.grid x y (color+1)
-
-    moveAnt turnedAnt
-
 let saveAs (name : string) (bitmap : Bitmap) =
-    let path = Path.Combine(__SOURCE_DIRECTORY__, name)
+    let path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, name)
     bitmap.Save(path)
-
-let rec callWork (ant : ant) (n : int) : ant =
-    match n with
-    | x when x > 1 -> callWork (workAnt ant) (n-1)
-    | x when x = 1 -> (workAnt ant)
-    | _ -> (workAnt ant)
-
-let final = callWork initialAnt iterations
-
-let getRGBColor (value : int) (ant : ant) =
-    let rgb = (Seq.item value colors).To<Rgb>()
-    let rgbColor = Color.FromArgb(255, int(rgb.R), int(rgb.G), int(rgb.B))
-    rgbColor
 
 let outputBitmap ant =
     let outputBitmap =
         new Bitmap(1000, 1000)
 
+    let getColor index =
+        Seq.item index ant.colors
+
     let pixelFunc x y v =
-        if v <> 0 then
-            printfn "%A" v
-        outputBitmap.SetPixel(x, y, (getRGBColor v ant))
+        outputBitmap.SetPixel(x, y, getColor v)
+    
     let outputGrid =
         Array2D.iteri pixelFunc ant.grid
     outputBitmap
 
-saveAs "langton.png" (outputBitmap final)
+
+let executeAnt xSize ySize (directions : float []) iterations orientation outputName =
+    let initialAnt = {
+        dimensions = (xSize, ySize)
+        position = (xSize / 2 - 1, ySize / 2 - 1)
+        colors = makeColorPalette directions
+        vector = orientation
+        grid = Array2D.zeroCreate<int> xSize ySize }
+
+    let workAnt ant =
+        let { position = position } = ant
+        let (x, y) = position
+        let color = Array2D.get ant.grid x y
+        let turnedAnt = addAngle ant directions.[color]
+
+        if color >= directions.Length-1 then
+            Array2D.set ant.grid x y 0
+        else
+            Array2D.set ant.grid x y (color+1)
+
+        moveAnt turnedAnt
+
+    let rec callWork (ant : ant) (n : int) : ant =
+        match n with
+        | x when x > 1 -> callWork (workAnt ant) (n-1)
+        | x when x = 1 -> (workAnt ant)
+        | _ -> (workAnt ant)
+
+    let final = callWork initialAnt iterations
+    saveAs outputName (outputBitmap final)
--- LangtonsAnt/Program.fs
+++ LangtonsAnt/Program.fs
@@ -4,5 +4,29 @@ open Ant
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+    let convertDirs (ds:string) =
+        ds.ToCharArray()
+        |> Array.map (fun x -> match x with
+                               | 'L' -> 90.0
+                               | 'R' -> -90.0
+                               | _ -> 0.0)
+
+    let convertOrientation (os:string) =
+        match os with
+        | "N" -> 0.0
+        | "S" -> 180.0
+        | "W" -> 90.0
+        | "E" -> 270.0
+        | _ -> 0.0
+
+    let outputName (dirs : string) (iterations : string) =
+        sprintf "%s_%s.png" dirs iterations
+
+    if argv.Length = 3 then
+        let dirs : float [] = argv.[0] |> convertDirs
+        let iterations : int = argv.[1] |> int
+        let orientation : float = argv.[2] |> convertOrientation
+        ignore (executeAnt 500 500 dirs iterations orientation (outputName argv.[0] argv.[1]))
+        0
+    else
+        0 // return an integer exit code
